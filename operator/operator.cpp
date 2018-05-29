9da17ea455900e6a96f3d72b9825d6d654bd11a0
20172104244 <1060071702@qq.com>
2018-05-29 15:26:56 +0800

    英尺英寸的减法
    在 display里加了语句对inches 的正负作出判断
    在 operator也加了语句对feet 的正负作出判断

--- operator/operator.cpp
+++ operator/operator.cpp
@@ -17,30 +17,44 @@ public:
 };
 void CFeet::setvalue(int ft, int in)
 {
-	feet = ft + in / 12;
-	inches = in % 12;
+	feet = ft ;
+	inches = in ;
 }
 void CFeet::display()
 {
-	cout << feet << "英尺" << inches << "英寸" << endl;
+	if (inches < 0)
+	{
+		cout << inches / 12 << "英尺" << -inches % 12 << "英寸" << endl;
+	}
+	if (inches > 0)
+	{
+		cout << inches / 12 << "英尺" << inches % 12 << "英寸" << endl;
+	}
 }
-CFeet CFeet::add(CFeet & objf)  //objf是对象引用b
+/*CFeet CFeet::add(CFeet & objf)  //objf是对象引用b
 {
 	CFeet temp;
 	temp.setvalue(feet + objf.feet, inches + objf.inches);
 	return temp;
-}
+}*/
 CFeet CFeet::operator+(CFeet & objf)  
 {
 	CFeet temp;
-	temp.setvalue(feet + objf.feet, inches + objf.inches);
+	if (feet > 0)
+	{
+		temp.setvalue(0, inches = (feet * 12 + inches) - (objf.feet * 12 + objf.inches));
+	}
+	else
+	{
+		temp.setvalue(0, inches = (feet * 12 - inches) - (objf.feet * 12 + objf.inches));
+	}
 	return temp;
 }
 int main()
 {
 	CFeet A, B, C;
-	A.setvalue(10,11 );
-	B.setvalue(6,9 );
+	A.setvalue(-2,3 );
+	B.setvalue(2,3 );
 	C = A + B; //有CFeet CFeet::operator+(CFeet & objf)的时候能用c=a+b ，有CFeet CFeet::add(CFeet & objf)时用c=a.add(b)
 	C.display();
     return 0;
