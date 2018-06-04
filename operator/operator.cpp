159ca8d5ad8da4a9521e6aa949a86ddb9c0832fd
20172104244 <1060071702@qq.com>
2018-06-04 17:28:48 +0800

    operator
    把display里的if语句减掉，因为display里只可以有cout。另加了一个change 函数，对feet和inches 进行了改变。由于这是英寸英尺的相减所以把+变成了-。

--- operator/operator.cpp
+++ operator/operator.cpp
@@ -12,50 +12,59 @@ private:
 public:
 	void setvalue(int ft, int in);
 	void display();
-	CFeet add(CFeet & objf);
-	CFeet operator+(CFeet & objf);
+	void change();
+	//CFeet add(CFeet & objf);
+	CFeet operator-(CFeet & objf);
 };
 void CFeet::setvalue(int ft, int in)
 {
 	feet = ft ;
 	inches = in ;
 }
-void CFeet::display()
-{
-	if (inches < 0)
-	{
-		cout << inches / 12 << "英尺" << -inches % 12 << "英寸" << endl;
-	}
-	if (inches > 0)
-	{
-		cout << inches / 12 << "英尺" << inches % 12 << "英寸" << endl;
-	}
-}
+
 /*CFeet CFeet::add(CFeet & objf)  //objf是对象引用b
 {
 	CFeet temp;
 	temp.setvalue(feet + objf.feet, inches + objf.inches);
 	return temp;
 }*/
-CFeet CFeet::operator+(CFeet & objf)  
+CFeet CFeet::operator-(CFeet & objf)  
 {
 	CFeet temp;
 	if (feet > 0)
 	{
-		temp.setvalue(0, inches = (feet * 12 + inches) - (objf.feet * 12 + objf.inches));
+		temp.setvalue(0,  (feet * 12 + inches) - (objf.feet * 12 + objf.inches));
 	}
 	else
 	{
-		temp.setvalue(0, inches = (feet * 12 - inches) - (objf.feet * 12 + objf.inches));
+		temp.setvalue(0,  (feet * 12 - inches) - (objf.feet * 12 + objf.inches));
 	}
 	return temp;
+}
+void CFeet::change()
+{
+	if (inches < 0)
+	{
+		feet = inches / 12;
+		inches = -inches % 12;
+	}
+	else
+	{
+		feet = inches / 12;
+		inches = inches % 12;
+	}
+}
+void CFeet::display()
+{
+	cout << feet << "英尺" << inches << "英寸" << endl;
 }
 int main()
 {
 	CFeet A, B, C;
-	A.setvalue(-2,3 );
-	B.setvalue(2,3 );
-	C = A + B; //有CFeet CFeet::operator+(CFeet & objf)的时候能用c=a+b ，有CFeet CFeet::add(CFeet & objf)时用c=a.add(b)
+	A.setvalue(2,3 );
+	B.setvalue(1,3 );
+	C = A - B; //有CFeet CFeet::operator+(CFeet & objf)的时候能用c=a+b ，有CFeet CFeet::add(CFeet & objf)时用c=a.add(b)
+	C.change();
 	C.display();
     return 0;
 }
